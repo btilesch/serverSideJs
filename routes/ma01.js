2728efbc03a23777dc0185e8d762a25691953311
Tilesch Bálint <tilesch.balint@gammadigital.hu>
2024-04-21 20:23:03 +0200

    file renames

--- routes/ma01.js
+++ routes/ma01.js
@@ -1,11 +1,16 @@
 const renderMW = require('../middleware/renderMW');
+const saveMa01MW = require('../middleware/saveMa01MW');
+
+const MA01Model = require('../models/ma01');
 
 module.exports = function (app) {
-  let objectRepository = {};
+  let objectRepository = {
+    MA01Model: MA01Model,
+  };
 
-  app.use('/ma01/new', renderMW(objectRepository, 'ma01Edit'));
+  app.use('/ma01/new', saveMa01MW(objectRepository), renderMW(objectRepository, 'ma01Edit'));
 
-  app.use('/ma01/edit', renderMW(objectRepository, 'ma01Edit'));
+  app.use('/ma01/edit', saveMa01MW(objectRepository), renderMW(objectRepository, 'ma01Edit'));
 
   app.use('/ma01/details', renderMW(objectRepository, 'ma01Details'));
 

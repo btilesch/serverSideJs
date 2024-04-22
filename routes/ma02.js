ac3811d32e32df22d4c8a12c8f39d5933f59b668
Tilesch Bálint <tilesch.balint@gammadigital.hu>
2024-04-23 01:11:32 +0200

    middlewares for ma02

--- routes/ma02.js
+++ routes/ma02.js
@@ -1,16 +1,36 @@
 const renderMW = require('../middleware/renderMW');
+const saveMA02MW = require('../middleware/ma02/saveMA02MW');
+const getMA02ListMW = require('../middleware/ma02/getMA02ListMW');
+const getMA02MW = require('../middleware/ma02/getMA02MW');
+const deleteMA02MW = require('../middleware/ma02/deleteMA02MW');
+const enableMA02MW = require('../middleware/ma02/enableMA02MW');
+
+const MA02Model = require('../models/ma02');
 
 module.exports = function (app) {
-  let objectRepository = {};
+  let objectRepository = {
+    MA02Model: MA02Model,
+  };
 
-  app.get('/ma02/new', renderMW(objectRepository, 'ma02Edit'));
-  app.post('/ma02/new', renderMW(objectRepository, 'ma02'));
+  app.use('/ma02/new', saveMA02MW(objectRepository), renderMW(objectRepository, 'ma02Edit'));
 
-  app.get('/ma02/edit', renderMW(objectRepository, 'ma02Edit'));
-  app.use('/ma02/edit', renderMW(objectRepository, 'ma02'));
+  app.use(
+    '/ma02/edit/:ma02Id',
+    getMA02MW(objectRepository),
+    saveMA02MW(objectRepository),
+    renderMW(objectRepository, 'ma02Edit'),
+  );
 
-  app.use('/ma02/details', renderMW(objectRepository, 'ma02Details'));
-  app.use('/ma02/details', renderMW(objectRepository, 'ma02Details'));
+  app.use('/ma02/delete/:ma02Id', deleteMA02MW(objectRepository));
 
-  app.use('/ma02', renderMW(objectRepository, 'ma02'));
+  app.use(
+    '/ma02/enable/:ma02Id',
+    getMA02MW(objectRepository),
+    enableMA02MW(objectRepository),
+    renderMW(objectRepository, 'ma02'),
+  );
+
+  app.use('/ma02/:ma02Id', getMA02MW(objectRepository), renderMW(objectRepository, 'ma02Details'));
+
+  app.use('/ma02', getMA02ListMW(objectRepository), renderMW(objectRepository, 'ma02'));
 };

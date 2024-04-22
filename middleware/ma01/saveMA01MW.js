8d98d93d519725aa1eac5b99da8dbc04d613c529
Tilesch Bálint <tilesch.balint@gammadigital.hu>
2024-04-23 01:11:54 +0200

    mongoose api refactor

--- middleware/ma01/saveMA01MW.js
+++ middleware/ma01/saveMA01MW.js
@@ -4,7 +4,7 @@ const createHttpError = require('http-errors');
 module.exports = function (objectrepository) {
   const MA01Model = requireOption(objectrepository, 'MA01Model');
 
-  return async function (req, res, next) {
+  return function (req, res, next) {
     if (
       typeof req.body.name === 'undefined' ||
       typeof req.body.tempLow === 'undefined' ||
@@ -18,24 +18,25 @@ module.exports = function (objectrepository) {
       res.locals.ma01 = new MA01Model();
     }
 
-    try {
-      res.locals.ma01.name = req.body.name;
-      res.locals.ma01.enabled = req.body.enabled ?? false;
-      res.locals.ma01.creationDate = Date.now();
-      res.locals.ma01.username = 'Unknown'; //TODO: read from session
-      res.locals.ma01.tempLow = req.body.tempLow;
-      res.locals.ma01.tempHigh = req.body.tempHigh;
-      res.locals.ma01.powerMax = req.body.powerMax;
+    res.locals.ma01.name = req.body.name;
+    res.locals.ma01.enabled = req.body.enabled ?? false;
+    res.locals.ma01.creationDate = Date.now();
+    res.locals.ma01.username = 'Unknown'; //TODO: read from session
+    res.locals.ma01.tempLow = req.body.tempLow;
+    res.locals.ma01.tempHigh = req.body.tempHigh;
+    res.locals.ma01.powerMax = req.body.powerMax;
 
-      const saved = await res.locals.ma01.save();
+    res.locals.ma01
+      .save()
+      .then((saved) => {
+        if (saved !== res.locals.ma01) {
+          return next(createHttpError(500, 'The save operation was unsuccessful'));
+        }
 
-      if (saved !== res.locals.ma01) {
-        return next(createHttpError(500, 'The save operation was unsuccessful'));
-      }
-
-      return res.redirect('/ma01');
-    } catch (err) {
-      next(err);
-    }
+        return res.redirect('/ma01');
+      })
+      .catch((err) => {
+        next(err);
+      });
   };
 };
